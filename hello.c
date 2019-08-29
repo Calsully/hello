3a234e183afc63262f1808e58b25af4c1728a13b
Calsully <csullivan16@huskers.unl.edu>
2019-08-29 11:23:55 -0500

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,16 @@
+/*
+Calvin Sullivan
+8-29-19
+Prints major and name
+*/
+
+#include <stblib.h>
+#include <stbio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Calvin Sullivan\n");
+  printf("Computer Engineering\n");
+
+  return 0;
+}
